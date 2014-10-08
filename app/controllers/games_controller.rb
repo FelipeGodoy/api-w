18c744389cb686568a23eebb25ea6c7a907a1248
jefferson.lessa <jefferson2459@gmail.com>
2014-10-08 09:39:05 -0300

    Criacao do controller game

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -4,6 +4,8 @@ class GamesController < ApplicationController
   end
 
   def new
+    ##url = localhost.ntp.uff.br:3000/games/new?nome=war1&jogador=jefferson&qtd_ter=42&qtd_obj=3&qtd_jog=3
+
     if params
       dados_jogo = {
           nome: params[:nome],
@@ -12,7 +14,7 @@ class GamesController < ApplicationController
           qtd_objetivos: params[:qtd_obj],
           qtd_jogadores: params[:qtd_jog]
       }
-
+      render json: dados_jogo.to_json
     end
   end
 end
