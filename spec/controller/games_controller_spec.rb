c827f839a35ced751d3a480d3a07674ef274e392
jlessa <jefferson2459@gmail.com>
2014-11-06 18:28:23 -0200

    Instalação da GEM para test

---
+++ spec/controller/games_controller_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+
+RSpec.describe GamesController, :type => :controller do
+  describe "GET" do
+    it "sem parametros" do
+      get :index
+      expect(response).to be_success
+    end
+  end
+end
